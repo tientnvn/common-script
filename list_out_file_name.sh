64f858da4a8a41051198562e72a2352faa37b07c
Tien Nguyen <toantien.vn91@gmail.com>
2017-02-21 18:28:11 +0700

    Add list out file name script

---
+++ list_out_file_name.sh
@@ -0,0 +1,28 @@
+MYPATH=$1
+OUTPUT_DIRECTORY=$2
+
+if [ ! -d "$OUTPUT_DIRECTORY" ]; then
+  mkdir $OUTPUT_DIRECTORY
+fi
+
+
+numbers=($(ls -d $MYPATH/*))
+for (( i=0; i<${#numbers[@]}; i++ ));
+do
+  parent=($(ls -d -1 ${numbers[i]}/*))
+  # echo $parent
+  category=$(basename ${numbers[i]})
+  echo $category
+  filename=$category.txt
+  for (( j=0; j<${#parent[@]}; j++ ));
+  do
+    # echo ${parent[j]};
+    file=$(basename ${parent[j]});
+    echo $file
+    TAP=j+1
+    echo $file >> $OUTPUT_DIRECTORY/$filename
+    echo "Tập $TAP" >> $OUTPUT_DIRECTORY/$filename
+    echo "" >> $OUTPUT_DIRECTORY/$filename
+    echo "" >> $OUTPUT_DIRECTORY/$filename
+  done
+done
