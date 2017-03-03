bbaef2c63d9754eccf2b0193763b92a892ce3b41
Tien Nguyen <toantien.vn91@gmail.com>
2017-03-03 10:39:59 +0700

    Add clone wordpress site script

---
+++ clone_wordpress_site.sh
@@ -0,0 +1,20 @@
+DOMAIN=$1
+APACHE_FOLDER=/etc/apache2/sites-available
+WWW_FOLDER=/var/www
+WORDPRESS_ORIGINAL_FOLDER=${WWW_FOLDER}/wordpress
+CONFIG_FILE=${DOMAIN}.conf
+
+# Clone wordpress code
+sudo cp -rf ${WORDPRESS_ORIGINAL_FOLDER} ${WWW_FOLDER}/${DOMAIN}
+
+# Replace actual name, port, domain
+sudo sed -e "s/\${domain}/${DOMAIN}/" apache_config_template.txt > ${APACHE_FOLDER}/${CONFIG_FILE}
+
+# Copy file to nginx folder
+sudo cp ${NAME} ${APACHE_FOLDER}
+
+# Enable the virtual host files that we created by typing
+sudo a2ensite ${CONFIG_FILE}
+
+# Restart apache2 service
+sudo service apache2 reload
