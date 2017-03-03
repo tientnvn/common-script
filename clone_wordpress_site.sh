cbd793d850cabeb156fbb00e8a97d0ad860d03e7
Tien Nguyen <toantien.vn91@gmail.com>
2017-03-03 10:54:19 +0700

    fix

--- clone_wordpress_site.sh
+++ clone_wordpress_site.sh
@@ -3,15 +3,13 @@ APACHE_FOLDER=/etc/apache2/sites-available
 WWW_FOLDER=/var/www
 WORDPRESS_ORIGINAL_FOLDER=${WWW_FOLDER}/wordpress
 CONFIG_FILE=${DOMAIN}.conf
+SCRIPT_FOLDER=/data/script/common-script
 
 # Clone wordpress code
 sudo cp -rf ${WORDPRESS_ORIGINAL_FOLDER} ${WWW_FOLDER}/${DOMAIN}
 
 # Replace actual name, port, domain
-sudo sed -e "s/\${domain}/${DOMAIN}/" apache_config_template.txt > ${APACHE_FOLDER}/${CONFIG_FILE}
-
-# Copy file to nginx folder
-sudo cp ${NAME} ${APACHE_FOLDER}
+sudo sed -e "s/\${domain}/${DOMAIN}/" ${SCRIPT_FOLDER}/apache_config_template.txt > ${APACHE_FOLDER}/${CONFIG_FILE}
 
 # Enable the virtual host files that we created by typing
 sudo a2ensite ${CONFIG_FILE}
